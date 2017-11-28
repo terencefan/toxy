fc076df351a432cbdebbbf3dc82c9f6a0950bcd6
stdrickforce <stdrickforce@gmail.com>
2017-11-28 11:55:06 +0800

    tenchmark framework

--- tenchmark/main.go
+++ tenchmark/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+
+	. "github.com/stdrickforce/thriftgo/protocol"
+)
+
+var wg sync.WaitGroup
+
+func call(proto Protocol, name string, args ...interface{}) error {
+	return nil
+}
+
+func processor(id int, fn func(proto Protocol) error) {
+	defer wg.Done()
+
+	// TODO build a real protocol
+	var proto Protocol
+
+	for i := 0; i < 20; i++ {
+		fn(proto)
+		fmt.Println(id, i)
+	}
+}
 
 func main() {
-	fmt.Println("tenchmark")
+	for i := 0; i < 10; i++ {
+		fn := func(proto Protocol) error {
+			return call(proto, "ping")
+		}
+		go processor(i, fn)
+		wg.Add(1)
+	}
+	wg.Wait()
 }
